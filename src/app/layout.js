2cab96030849711dd8cca898afc9472925a0c19c
Ice\UNA <icebenitez@gmail.com>
2025-05-17 22:15:56 +0800

    refactor: group layouts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
-import Navbar from "../../components/Navbar";
-import Footer from "../../components/Footer";
-import "../globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import "./globals.css";
 
 export const metadata = {
   title: "Home - Kapehan",
